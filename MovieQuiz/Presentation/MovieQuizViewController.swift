d3805224cc071a8e18d5ccc2dbecb8474dcee075
studiyaru <sergey.a@studiya.ru>
2025-02-27 20:10:42 +0300

    figma

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,11 +1,11 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    @IBOutlet weak var label: UILabel!
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.font = UIFont(name: "YSDisplay-Medium", size: 90)
+       // label.font = UIFont(name: "YSDisplay-Medium", size: 90)
     }
 }
 
